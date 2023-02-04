4dd7db7cc6f181ab600f872e2a21df8e1dc1f6a0
Fire Island Robotics <fireislandrobotics@gmail.com>
2023-02-04 16:51:09 -0600

    working!!! need to implement pigeon2

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,139 @@
+package frc.robot;
+
+import com.revrobotics.CANSparkMax.IdleMode;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import frc.lib.config.SwerveModuleConstants;
+
+public final class Constants {
+
+  public static final class Swerve {
+    public static final double stickDeadband = 0.2;
+
+    public static final boolean invertGyro = false; // Always ensure Gyro is CCW+ CW-
+
+    /* Drivetrain Constants */
+    public static final double trackWidth = Units.inchesToMeters(20.75);
+    public static final double wheelBase = Units.inchesToMeters(21.75);
+    public static final double wheelDiameter = Units.inchesToMeters(4.0);
+    public static final double wheelCircumference = wheelDiameter * Math.PI;
+
+    public static final double openLoopRamp = 0.25;
+    public static final double closedLoopRamp = 0.0;
+
+    public static final double driveGearRatio = (50.0/14.0) * (17.0/27.0) * (45.0/15.0); // 6.75:1
+    public static final double angleGearRatio = (150.0 / 7.0); // 
+
+    public static final SwerveDriveKinematics swerveKinematics =
+        new SwerveDriveKinematics(
+            new Translation2d(wheelBase / 2.0, trackWidth / 2.0),
+            new Translation2d(wheelBase / 2.0, -trackWidth / 2.0),
+            new Translation2d(-wheelBase / 2.0, trackWidth / 2.0),
+            new Translation2d(-wheelBase / 2.0, -trackWidth / 2.0));
+
+    /* Swerve Voltage Compensation */
+    public static final double voltageComp = 12.0;
+
+    /* Swerve Current Limiting */
+    public static final int angleContinuousCurrentLimit = 20;
+    public static final int driveContinuousCurrentLimit = 80;
+
+    /* Angle Motor PID Values */
+    public static final double angleKP = 0.01;
+    public static final double angleKI = 0.0;
+    public static final double angleKD = 0.0;
+    public static final double angleKFF = 0.0;
+
+    /* Drive Motor PID Values */
+    public static final double driveKP = 0.1;
+    public static final double driveKI = 0.0;
+    public static final double driveKD = 0.0;
+    public static final double driveKFF = 0.0;
+
+    /* Drive Motor Characterization Values */
+    public static final double driveKS = 0.667;
+    public static final double driveKV = 2.44;
+    public static final double driveKA = 0.27;
+
+    /* Drive Motor Conversion Factors */
+    public static final double driveConversionPositionFactor =
+        (wheelDiameter * Math.PI) / driveGearRatio;
+    public static final double driveConversionVelocityFactor = driveConversionPositionFactor / 60.0;
+    public static final double angleConversionFactor = 360.0 / angleGearRatio;
+
+    /* Swerve Profiling Values */
+    public static final double maxSpeed = 4.5; // meters per second  //4.5
+    public static final double maxAngularVelocity = 6;            // 11.5
+
+    /* Neutral Modes */
+    public static final IdleMode angleNeutralMode = IdleMode.kBrake;
+    public static final IdleMode driveNeutralMode = IdleMode.kBrake;
+
+    /* Motor Inverts */
+    public static final boolean driveInvert = true;
+    public static final boolean angleInvert = true;
+
+    /* Angle Encoder Invert */
+    public static final boolean canCoderInvert = false;
+
+    /* Module Specific Constants */
+    /* Front Left Module - Module 0 */
+    public static final class Mod0 {
+      public static final int driveMotorID = 1;
+      public static final int angleMotorID = 2;
+      public static final int canCoderID = 21;
+      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(38+180); //108.28-90-0.8
+      public static final SwerveModuleConstants constants =
+          new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
+    }
+
+    /* Front Right Module - Module 1 */
+    public static final class Mod1 {
+      public static final int driveMotorID = 3;
+      public static final int angleMotorID = 4;
+      public static final int canCoderID = 22;
+      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(117+180); //79.98-90-1.5
+      public static final SwerveModuleConstants constants =
+          new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
+    }
+
+    /* Back Left Module - Module 2 */
+    public static final class Mod2 {
+      public static final int driveMotorID = 5;
+      public static final int angleMotorID = 6;
+      public static final int canCoderID = 23;
+      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(170+180);
+      public static final SwerveModuleConstants constants =
+          new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
+    }
+
+    /* Back Right Module - Module 3 */
+    public static final class Mod3 {
+      public static final int driveMotorID = 7;
+      public static final int angleMotorID = 8;
+      public static final int canCoderID = 24;
+      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(49+180);
+      public static final SwerveModuleConstants constants =
+          new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
+    }
+  }
+
+  public static final class AutoConstants {
+    public static final double kMaxSpeedMetersPerSecond = 4; //3
+    public static final double kMaxAccelerationMetersPerSecondSquared = 3; //3
+    public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
+    public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
+
+    public static final double kPXController = 1;
+    public static final double kPYController = 1;
+    public static final double kPThetaController = 1;
+
+    // Constraint for the motion profilied robot angle controller
+    public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
+        new TrapezoidProfile.Constraints(
+            kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
+  }
+}
---
+++ src/main/java/frc/robot/autos/exampleAuto.java
@@ -0,0 +1,180 @@
+package frc.robot.autos;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.commands.TurnToAngleCommand;
+import frc.robot.subsystems.Swerve;
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPoint;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+
+public class exampleAuto extends SequentialCommandGroup {
+  public exampleAuto(Swerve s_Swerve) {
+
+
+    // PathPlannerTrajectory trajectory1 = PathPlanner.generatePath(
+    //   new PathConstraints(AutoConstants.kMaxSpeedMetersPerSecond,
+    //                       AutoConstants.kMaxAccelerationMetersPerSecondSquared), 
+    //   new PathPoint(new Translation2d(0,0), Rotation2d.fromDegrees(180), Rotation2d.fromDegrees(0)),
+    //   new PathPoint(new Translation2d(-1.5,0), Rotation2d.fromDegrees(180), Rotation2d.fromDegrees(90)),
+    //   new PathPoint(new Translation2d(-3,0), Rotation2d.fromDegrees(180), Rotation2d.fromDegrees(180)),
+    //   new PathPoint(new Translation2d(-5,0), Rotation2d.fromDegrees(180), Rotation2d.fromDegrees(180))
+    // );
+    // PathPlannerTrajectory trajectory2 = PathPlanner.generatePath(
+    //   new PathConstraints(AutoConstants.kMaxSpeedMetersPerSecond,
+    //                       AutoConstants.kMaxAccelerationMetersPerSecondSquared), 
+    //   new PathPoint(new Translation2d(-5,0), Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(180)),
+    //   new PathPoint(new Translation2d(-3.5,0), Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(270)),
+    //   new PathPoint(new Translation2d(-2,0), Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(360)),
+    //   new PathPoint(new Translation2d(0,0), Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(360))
+    // );
+
+
+    // PathPlannerTrajectory trajectory1 = PathPlanner.generatePath(
+    //   new PathConstraints(AutoConstants.kMaxSpeedMetersPerSecond,
+    //                       AutoConstants.kMaxAccelerationMetersPerSecondSquared), 
+    //   new PathPoint(new Translation2d(0,0), Rotation2d.fromDegrees(180), Rotation2d.fromDegrees(0)),
+    //   new PathPoint(new Translation2d(-5,0), Rotation2d.fromDegrees(180), Rotation2d.fromDegrees(0))
+    // );
+    // PathPlannerTrajectory trajectory2 = PathPlanner.generatePath(
+    //   new PathConstraints(AutoConstants.kMaxSpeedMetersPerSecond,
+    //                       AutoConstants.kMaxAccelerationMetersPerSecondSquared), 
+    //   new PathPoint(new Translation2d(-5,0), Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(0)),
+    //   new PathPoint(new Translation2d(0,0), Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(0))
+    // );
+
+    PathPlannerTrajectory trajectory1 = PathPlanner.generatePath(
+      new PathConstraints(AutoConstants.kMaxSpeedMetersPerSecond,
+                          AutoConstants.kMaxAccelerationMetersPerSecondSquared), 
+      new PathPoint(new Translation2d(0,0), Rotation2d.fromDegrees(180), Rotation2d.fromDegrees(0)),
+      new PathPoint(new Translation2d(-1,0), Rotation2d.fromDegrees(180), Rotation2d.fromDegrees(0))
+    );
+    PathPlannerTrajectory trajectory2 = PathPlanner.generatePath(
+      new PathConstraints(AutoConstants.kMaxSpeedMetersPerSecond,
+                          AutoConstants.kMaxAccelerationMetersPerSecondSquared), 
+      new PathPoint(new Translation2d(-1,0), Rotation2d.fromDegrees(180), Rotation2d.fromDegrees(180)),
+      new PathPoint(new Translation2d(-5,0), Rotation2d.fromDegrees(180), Rotation2d.fromDegrees(180))
+    );
+    PathPlannerTrajectory trajectory3 = PathPlanner.generatePath(
+      new PathConstraints(AutoConstants.kMaxSpeedMetersPerSecond,
+                          AutoConstants.kMaxAccelerationMetersPerSecondSquared), 
+      new PathPoint(new Translation2d(-5,0), Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(180)),
+      new PathPoint(new Translation2d(-1,0), Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(180))
+    );
+    PathPlannerTrajectory trajectory4 = PathPlanner.generatePath(
+      new PathConstraints(AutoConstants.kMaxSpeedMetersPerSecond,
+                          AutoConstants.kMaxAccelerationMetersPerSecondSquared), 
+      new PathPoint(new Translation2d(-1,0), Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(0)),
+      new PathPoint(new Translation2d(0,0), Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(0))
+    );
+    // PathPlannerTrajectory trajectory2 = PathPlanner.generatePath(
+    //   new PathConstraints(AutoConstants.kMaxSpeedMetersPerSecond,
+    //                       AutoConstants.kMaxAccelerationMetersPerSecondSquared), 
+    //   new PathPoint(new Translation2d(-5,0), Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(0)),
+    //   new PathPoint(new Translation2d(0,0), Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(0))
+    // );
+
+    // // An example trajectory to follow.  All units in meters.
+    // Trajectory exampleTrajectory =
+    //     TrajectoryGenerator.generateTrajectory(
+    //         // Start at the origin facing the +X direction
+    //         new Pose2d(0, 0, new Rotation2d(0)),
+    //         // Pass through these two interior waypoints, making an 's' curve path
+    //         List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+    //         // End 3 meters straight ahead of where we started, facing forward
+    //         new Pose2d(3, 0, new Rotation2d(0)),
+    //         config);
+
+    var thetaController =
+        new ProfiledPIDController(
+            Constants.AutoConstants.kPThetaController,
+            0,
+            0,
+            Constants.AutoConstants.kThetaControllerConstraints);
+    thetaController.enableContinuousInput(0, 2*Math.PI);
+
+    PPSwerveControllerCommand swerveControllerCommand1 =
+        new PPSwerveControllerCommand(
+            trajectory1,
+            s_Swerve::getPose,
+            Constants.Swerve.swerveKinematics,
+            new PIDController(Constants.AutoConstants.kPXController, 0, 0),
+            new PIDController(Constants.AutoConstants.kPYController, 0, 0),
+            new PIDController(Constants.AutoConstants.kPThetaController, 0, 0),
+            s_Swerve::setModuleStates,
+            s_Swerve);
+    PPSwerveControllerCommand swerveControllerCommand2 =
+          new PPSwerveControllerCommand(
+              trajectory2,
+              s_Swerve::getPose,
+              Constants.Swerve.swerveKinematics,
+              new PIDController(Constants.AutoConstants.kPXController, 0, 0),
+              new PIDController(Constants.AutoConstants.kPYController, 0, 0),
+              new PIDController(Constants.AutoConstants.kPThetaController, 0, 0),
+              s_Swerve::setModuleStates,
+              s_Swerve);
+          PPSwerveControllerCommand swerveControllerCommand3 =
+          new PPSwerveControllerCommand(
+              trajectory3,
+              s_Swerve::getPose,
+              Constants.Swerve.swerveKinematics,
+              new PIDController(Constants.AutoConstants.kPXController, 0, 0),
+              new PIDController(Constants.AutoConstants.kPYController, 0, 0),
+              new PIDController(Constants.AutoConstants.kPThetaController, 0, 0),
+              s_Swerve::setModuleStates,
+              s_Swerve);
+      PPSwerveControllerCommand swerveControllerCommand4 =
+            new PPSwerveControllerCommand(
+                trajectory4,
+                s_Swerve::getPose,
+                Constants.Swerve.swerveKinematics,
+                new PIDController(Constants.AutoConstants.kPXController, 0, 0),
+                new PIDController(Constants.AutoConstants.kPYController, 0, 0),
+                new PIDController(Constants.AutoConstants.kPThetaController, 0, 0),
+                s_Swerve::setModuleStates,
+                s_Swerve);
+
+    addCommands(
+      //new InstantCommand(() -> s_Swerve.resetOdometry(trajectory1.getInitialPose())),
+      new InstantCommand(() -> s_Swerve.resetOdometry(new Pose2d(new Translation2d(0,0), Rotation2d.fromDegrees(0)))),
+      swerveControllerCommand1,
+      new TurnToAngleCommand(s_Swerve, 180, 2),
+      swerveControllerCommand2,
+      new InstantCommand(() -> s_Swerve.drive(new Translation2d(0,0), 0, false, true)),
+      new WaitCommand(1),
+      swerveControllerCommand3,
+      new TurnToAngleCommand(s_Swerve, 0, 2),
+      swerveControllerCommand4,
+      new InstantCommand(() -> s_Swerve.drive(new Translation2d(0,0), 0, false, true))
+
+      // ,
+      // new InstantCommand(() -> s_Swerve.drive(new ChassisSpeeds(0,0,0)))
+    );
+
+    // addCommands(
+    //   //new InstantCommand(() -> s_Swerve.resetOdometry(trajectory1.getInitialPose())),
+    //   new InstantCommand(() -> s_Swerve.resetOdometry(new Pose2d(new Translation2d(0,0), Rotation2d.fromDegrees(0)))),
+    //   swerveControllerCommand1,
+    //   new InstantCommand(() -> s_Swerve.drive(new Translation2d(0,0), 0, false, true)),
+    //   new WaitCommand(1),
+    //   new InstantCommand(() -> s_Swerve.drive(new Translation2d(0,0), 0, false, true)),
+    //   swerveControllerCommand2
+    //   // ,
+    //   // new InstantCommand(() -> s_Swerve.drive(new ChassisSpeeds(0,0,0)))
+    // );
+
+    // addCommands(
+    //     new InstantCommand(() -> s_Swerve.resetOdometry(exampleTrajectory.getInitialPose())),
+    //     swerveControllerCommand);
+  }
+}
